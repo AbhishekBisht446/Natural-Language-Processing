8fe0e6090d5fdfaf29d291e15ea5b42085e257fd
AbhishekBisht446 <66470362+AbhishekBisht446@users.noreply.github.com>
2020-06-19 20:23:47 +0530

    Natural Language Processing
    The model to predict the reviews of a restaurant given in Restaurant_reviews.tsv file. The classification model used to predict is Naive_bayes with prediction of 0.73 which is pretty good.

--- Natural Language Processing.py
+++ Natural Language Processing.py
@@ -25,7 +25,28 @@ for i in range(0,1000):
     corpus.append(review)
 
 
-#crating the bag of word of model
+#creating the bag of word of model
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features = 1500)
-X = cv.fit_transform(corpus).toarray()
+X = cv.fit_transform(corpus).toarray() #converting corpus into sparse matrix
+
+
+#splitting dataset
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size = 0.1,random_state=0)#test set with size 100
+
+
+#Fitting Logistic Regression to the Training Set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train,Y_train)
+
+#predicting the test set resulting  
+y_pred = classifier.predict(X_test)
+
+
+#making Confusing Matrixx
+from sklearn.metrics import confusion_matrix 
+cm = confusion_matrix(Y_test,y_pred)
+
+#checkout the accuracy by (total correct prediction)/test set(100)
